fcc224a6654890e9dbdfefeb607b2721f1a9f534
Ozan Öğreden <oguzhan@oguzhanogreden.com>
2022-02-05 20:23:21 +0100

    refactor: clean up data service

--- timer-app/src/app/services/data.service.ts
+++ timer-app/src/app/services/data.service.ts
@@ -18,6 +18,9 @@ export type TimerDto = { id: string; name: string; startedAt: number; stoppedAt?
 export class DataService {
   _store = new BehaviorSubject<ServiceState>(_state);
   storeUpdated$ = this._store.asObservable();
+
+  timers$ = this._store.pipe(map(s => s.timers));
+
   constructor(private stateService: StateService) {
     this.readTimersFromStateOnConstruction()
       .subscribe(timers => {
@@ -29,41 +32,26 @@ export class DataService {
       next: store => this.writeStoreToState(store)
     });
   }
-  // TODO: getTimer()
 
   addTimer(timer: Timer) {
-    return combineLatest([
-      timer.name$,
-      timer.startedAt$.pipe(map((s) => s.toMillis())),
-      timer.id$,
-    ])
-      .pipe(
-        map(([name, startedAt, id]) => ({
-            id: id,
-            name: name,
-            startedAt: startedAt,
-          } as TimerState)
-        ),
-        tap(state => this.stateService.addTimer(state)),
-        filter(_ => false)
-      )
+    _state = {..._state, timers: [..._state.timers, timer] }
+
+    this._store.next(_state)
   }
-  
+    
   modifyTimer(timer: Timer) {
-      // timer.stoppedAt$.pipe(map(dt => dt?.toMillis())).subscribe(_ => console.log(_))
-    return zip(
-      timer.id$,
-      timer.name$,
-      timer.startedAt$.pipe(map(dt => dt.toMillis())),
-      timer.stoppedAt$.pipe(map(dt => dt?.toMillis()))
-    ).pipe(
-      // tap(_ => console.log(_)),
-      map(([id, name, startedAt, stoppedAt]) => ({id: id, name: "asd",stoppedAt: stoppedAt, startedAt: startedAt} as TimerUpdate)),
-      // tap(_ => console.log(_)),
-      switchMap(update => this.stateService.modifyTimer(update)), 
-      filter(_ => false))
+    const _timers = [
+      ..._state.timers.filter(t => t.id !== timer.id),
+      timer
+    ];
+
+    _state = {..._state, timers: _timers}
+    
+    console.log(_state);
+
+    this._store.next(_state);
   }
-  
+
   private readTimersFromStateOnConstruction() {
     return this.stateService.timers$.pipe(
       take(1),
@@ -81,12 +69,6 @@ export class DataService {
     )  
   }
   
-  private storedTimers() {}
-  
-  private timerToUpdate() {
-
-  }
-
   private writeStoreToState(store: ServiceState) {
     this.stateService.storeTimers(store.timers.map(x => ({
       id: x.id,
