4756b4adc422ba83fd9864f5edf2ce30b1f88c59
Ozan Öğreden <oguzhan@oguzhanogreden.com>
2022-02-08 20:54:22 +0100

    added conditional onboarding screen for platform == 'mobileweb'

--- timer-app/src/app/onboarding/onboarding.component.ts
+++ timer-app/src/app/onboarding/onboarding.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { Observable, Subject } from 'rxjs';
 import { NotificationService } from '../services/notification.service';
 import {
   User,
-  UserProfileService,
+  UserProfileService
 } from '../services/user/user-profile.service';
 
 @Component({
@@ -11,22 +12,25 @@ import {
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.scss'],
 })
-export class OnboardingComponent implements OnInit, OnDestroy {
+export class OnboardingComponent implements OnInit {
+  onIOsMobileWeb$ = new Subject<boolean>();
   userProfile: Observable<User>;
+
   constructor(
     private notificationService: NotificationService,
-    private userService: UserProfileService
+    private userService: UserProfileService,
+    private platform: Platform
   ) {
     this.userProfile = this.userService.user$;
+    
+    this.platform.ready().then(() => {
+      this.onIOsMobileWeb$.next(platform.is('mobileweb'));
+    })
   }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   requestPermissions() {
     this.notificationService.checkPermission();
-  }
-
-  ngOnDestroy(): void {
-    this.userProfile;
   }
 }
